2a43252027b85acfb2e449e24f2ffab62842bb66
vikasvarnekar3 <vikasvarnekar3@gmail.com>
2021-02-28 10:30:50 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        "HELLO WORLD",
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
