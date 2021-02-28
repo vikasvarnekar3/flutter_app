42ed7c059ee9ca71f5b7dfbac79033ae56462472
vikasvarnekar3 <vikasvarnekar3@gmail.com>
2021-02-28 11:05:07 +0530

    App Bar,style,color implimented

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-      child: Text(
-        "HELLO WORLD",
-        textDirection: TextDirection.ltr,
-      ),
-    ),
+      MaterialApp(
+          title: "My App",
+          home: Scaffold(
+            appBar:AppBar(title: Text("My App"),),
+          body: Material(
+            color: Colors.green,
+            child: Center(
+              child: Text(
+                "HELLO WORLD",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 40.0,color: Colors.amber),
+              ),
+            ),
+          )
+      )
+   )
   );
 }
