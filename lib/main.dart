2b3d77319da2929b4ef7b9d68c380085a534538d
vikasvarnekar3 <vikasvarnekar3@gmail.com>
2021-02-28 17:02:03 +0530

    debug icon remove ,uses of fat arrow and use of class

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-      MaterialApp(
-          title: "My App",
-          home: Scaffold(
-            appBar:AppBar(title: Text("My App"),),
-          body: Material(
-            color: Colors.green,
-            child: Center(
-              child: Text(
-                "HELLO WORLD",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(fontSize: 40.0,color: Colors.amber),
+void main() => runApp(myapp());//use of fat arrow and call the class
+
+class myapp extends StatelessWidget { // define the my app subclass and Statelesswidget
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: true, //remove debug batch
+        title: "My App",
+        home: Scaffold( //it is used for appBar
+            appBar: AppBar(title: Text("My App"),),
+            body: Material(
+              color: Colors.green, // background color
+              child: Center( //hello world prints at center
+                child: Text(
+                  "HELLO WORLD",
+                  textDirection: TextDirection.ltr, // text direaction left to right
+                  style: TextStyle(fontSize: 40.0, color: Colors.amber), // hello world font size and color
+                ),
               ),
-            ),
-          )
-      )
-   )
-  );
+            )
+        )
+    );
+  }
 }
